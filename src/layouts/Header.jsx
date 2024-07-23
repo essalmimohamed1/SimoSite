09ee01df616c6d3947faa61f320c8d94072610de
essalmimohamed1 <essalmimoha07@gmail.com>
2024-07-23 12:21:15 +0100

    édite the color

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -10,7 +10,7 @@ export const Header = () => {
     
 return (
     <div className="fixed top-0 w-[100vw]">
-        <Navbar expand="lg" className="bg-black bg-body-text-body-tertiary p-3 ">
+        <Navbar expand="lg" className="bg-black bg-body-text-body-tertiary p-3">
             <Container className='hello lg:flex lg:gap-80 sm:flex sm:flex-row sm:gap-0 lg:justify-between'>
                 <Navbar.Brand href="#home"><span className='text-4xl text-white font-extrabold w-[25vw]'><span className="text-blue-900">M</span>E<span className='text-blue-900'>.</span></span></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
--- src/pages/Home/components/section1.jsx
+++ src/pages/Home/components/section1.jsx
@@ -1,6 +1,6 @@
 import './section1.scss'
 import { TypeAnimation } from 'react-type-animation';
-import img1  from '../../../assets/images/WhatsApp Image 2024-02-21 at 16.07.18.jpeg'
+import img1  from '../../../assets/images/porfolio1.jpeg'
 import { FaHtml5 } from "react-icons/fa";
 import { FaCss3Alt } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io5";
@@ -19,9 +19,7 @@ import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-
-
-
+import { FaExternalLinkAlt } from 'react-icons/fa';
 
 
 
@@ -80,18 +78,28 @@ export const FirstSectionHome = () => {
         </div>
         <div className='d-lg-flex flex-lg-row flex-col gap-lg-0 gap-4 flex flex-col lg:items-center p-12 justify-around text-white bg-black'>
         <div >
-                <img src={img1} className='lg:w-[32vw] w-[100vw] rounded h-[50vh]' alt="" />
+                <img src={img1} className='lg:w-[29vw] w-[100vw] rounded h-[70vh]' alt="" />
             </div>
             <div className='flex flex-col gap-3'>
-                <h2 className='text-blue-900'>Full-Stack web Developer </h2>
-                <p className='lg:w-[40vw]' >I am Essalmi mohamed , a dedicated and passionate Web Developer with a strong background in crafting robust and user-centric web solutions. With 2ére of experience in the field, I bring a wealth of knowledge and expertise in front-end and front-end development, coupled with a keen eye for design and usability</p>
-                <div className='flex text-3xl gap-3  '>
-                    <FaGithub className='text-zinc-100 hover:text-yellow-600'/>
-                    <FaLinkedin className='text-blue-800 hover:text-yellow-600'/>
-                    <FaInstagram className='text-pink-700 hover:text-yellow-600'/>
-                    <FaXTwitter className='text-zinc-100 hover:text-yellow-600'/>
-                </div>
+            <h2 className='text-blue-900'>Full-Stack Web Developer</h2>
+            <p className='lg:w-[40vw]'>
+                I am Essalmi Mohamed, a dedicated and passionate Web Developer with a strong background in crafting robust and user-centric web solutions. With 2 years of experience in the field, I bring a wealth of knowledge and expertise in front-end and back-end development, coupled with a keen eye for design and usability.
+            </p>
+            <div className='flex text-3xl gap-3'>
+                <a href='https://github.com/essalmimohamed1' target='_blank' rel='noopener noreferrer'>
+                    <FaGithub className='text-zinc-100 hover:text-yellow-600' />
+                </a>
+                <a href='https://www.linkedin.com/in/mohamedessalmi/' target='_blank' rel='noopener noreferrer'>
+                    <FaLinkedin className='text-blue-800 hover:text-yellow-600' />
+                </a>
+                <a href='https://www.instagram.com/essalmisimo/' target='_blank' rel='noopener noreferrer'>
+                    <FaInstagram className='text-pink-700 hover:text-yellow-600' />
+                </a>
+                <a href='https://x.com/essalmi_si83203' target='_blank' rel='noopener noreferrer'>
+                    <FaXTwitter className='text-zinc-100 hover:text-yellow-600' />
+                </a>
             </div>
+        </div>
         </div>
         <div className='bg-black p-2 text-white text-center'>
             <h1>My <span className='text-blue-900'>SKILLS</span></h1>
@@ -138,27 +146,53 @@ export const FirstSectionHome = () => {
             </div>
         </div>
     </div>
-        <div className='bg-black p-2 text-white text-center'>
-            <h1>My <span className='text-red-600'>PROJECT</span></h1>
+    <div className='bg-black p-6'>
+            <div className='text-white text-center'>
+                <h1 className='text-4xl font-bold'>My <span className='text-blue-900'>PROJECTS</span></h1>
+            </div>
+            <div className='flex flex-wrap justify-around gap-5 mt-6'>
+                <div className='flex flex-col gap-3 items-center bg-gray-800 p-4 rounded-lg shadow-lg'>
+                    <h2 className='text-2xl font-semibold text-white'>Social Media Application</h2>
+                    <img src={img2} className='w-full max-w-[500px] border-4 border-blue-900 rounded-lg' alt="Project 1" />
+                    <div className='flex gap-4 mt-4'>
+                        <a href='https://github.com/your-repo-link' target='_blank' rel='noopener noreferrer' className='text-decoration-none'>
+                            <button className='flex items-center gap-2 border-2 border-blue-900 bg-blue-900 text-slate-50 p-2 rounded-2xl hover:bg-slate-600 hover:border-slate-600 hover:text-white transition duration-300'>
+                                <FaGithub /> View Code
+                            </button>
+                        </a>
+                        <a href='https://your-project-link' target='_blank' rel='noopener noreferrer' className='text-decoration-none'>
+                            <button className='flex items-center gap-2 border-2 border-white text-white p-2 rounded-2xl hover:bg-yellow-500 hover:text-white transition duration-300'>
+                                <FaExternalLinkAlt /> View Project
+                            </button>
+                        </a>
+                    </div>
+                </div>
+                <div className='flex flex-col gap-3 items-center bg-gray-800 p-4 rounded-lg shadow-lg'>
+                    <h2 className='text-2xl font-semibold text-white'>Web Shop</h2>
+                    <img src={img3} className='w-full max-w-[500px] border-4 border-blue-900 rounded-lg' alt="Project 2" />
+                    <div className='flex gap-4 mt-4'>
+                        <a href='https://github.com/your-repo-link' target='_blank' rel='noopener noreferrer' className='text-decoration-none'>
+                            <button className='flex items-center gap-2 border-2 border-blue-900 bg-blue-900 text-slate-50 p-2 rounded-2xl hover:bg-slate-600 hover:border-slate-600 hover:text-white transition duration-300'>
+                                <FaGithub /> View Code
+                            </button>
+                        </a>
+                        <a href='https://your-project-link' target='_blank' rel='noopener noreferrer' className='text-decoration-none'>
+                            <button className='flex items-center gap-2 border-2 border-white text-white p-2 rounded-2xl hover:bg-yellow-500 hover:text-white transition duration-300'>
+                                <FaExternalLinkAlt /> View Project
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-    <div className='bg-black p-6 d-lg-flex flex-lg-row flex-col gap-lg-0 gap-5 flex justify-around'>
-        <div className='flex flex-col gap-3 items-center justify-center'>
-            <img src={img2} className='lg:w-[40vw] border-4 border-red-600' alt="" />
-            <button className='border-1 border-white text-white p-2 rounded-2xl'>View project</button>
-        </div>
-        <div className='flex flex-col gap-3 items-center justify-center'>
-            <img src={img3} className='lg:w-[40vw] border-4 border-red-600' alt="" />
-            <button className='border-1 border-white text-white p-2 rounded-2xl'>View project</button>
-        </div>
-    </div>
     <div className='bg-black p-2 text-white text-center'>
-            <h1>Contact <span className='text-red-600'>Us</span></h1>
+            <h1>Contact <span className='text-blue-900'>Us</span></h1>
         </div>
         <form className="flex bg-black flex-col items-center justify-center gap-3 p-4 " ref={form} onSubmit={sendEmail}>
-                <input className="border-2 border-red-600 p-2 lg:w-[30vw] w-[80vw]  rounded-xl bg-black" type="text" name="to_name" placeholder='Your Name' />
-                <input className="border-2 border-red-600 p-2 lg:w-[30vw] w-[80vw]  rounded-xl bg-black" type="email" name="to_email" placeholder='Your Email'/>
-                <textarea className="border-2 border-red-600 p-2 h-60 w-[80vw] lg:w-[30vw] rounded-xl bg-black" name="message" placeholder='Your Message'/>
-                <input className="border-none bg-red-600 p-2 w-[80vw] lg:w-[15vw] text-white rounded-2xl"   type="submit" value="Send" />
+                <input className="border-2 border-blue-900 p-2 lg:w-[30vw] w-[80vw]  rounded-xl bg-black" type="text" name="to_name" placeholder='Your Name' />
+                <input className="border-2 border-blue-900 p-2 lg:w-[30vw] w-[80vw]  rounded-xl bg-black" type="email" name="to_email" placeholder='Your Email'/>
+                <textarea className="border-2 border-blue-900 p-2 h-60 w-[80vw] lg:w-[30vw] rounded-xl bg-black" name="message" placeholder='Your Message'/>
+                <input className="border-none bg-blue-900 p-2 w-[80vw] lg:w-[15vw] text-white rounded-2xl"   type="submit" value="Send" />
             </form>
     </>
     )
